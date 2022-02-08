40548b805774dab10915e789136fde59636fa1b0
Brandon Kambasha <btkambasha@gmail.com>
2022-02-08 10:25:58 +0000

    cheese pun on header

--- websites/default/nonpublic/sections/hero_section.php
+++ websites/default/nonpublic/sections/hero_section.php
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
           <h1>Welcome to Phillip's Cheese</h1>
-          <h2>We are team of professionals ready to fulfil your cheese Wishes</h2>
+          <h2>We are team of professionals ready to fulfil your cheese Wishes, so relax and take it cheesy!</h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
             <a href="" class="btn-get-started scrollto">Our Latest products</a>
             <a href="https://www.youtube.com/watch?v=AnRCxcmbQDE&ab_channel=CassandreUrsu" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Presentation video</span></a>
